aca8e26c0704d230d6cf995a452305040984e8e6
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-03-24 23:33:17 -0300

    Formatação

--- server/src/config/knex.js
+++ server/src/config/knex.js
@@ -2,13 +2,13 @@ import 'dotenv/config';
 import knexLib from 'knex';
 
 const knex = knexLib({
-  client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_ROOT_PASSWORD,
-    database: process.env.DB_DATABASE
-  }
+    client: 'mysql2',
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_ROOT_PASSWORD,
+        database: process.env.DB_DATABASE,
+    },
 });
 
 export default knex;

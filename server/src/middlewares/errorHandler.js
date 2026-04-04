efb7bca9af5036753227abf46ac7421e8752b37d
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-04-04 13:40:25 -0300

    Melhoria na middleware de tratamento de erros, evitando vazar informações

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -1,19 +1,23 @@
 function errorHandler(error, req, res, next) {
     const status = error.status || error.statusCode || 500;
 
+    // temporário, verificar pra depois colocar logs estruturados
+    console.log(error);
+
     // resposta padrão para caso de erro na request
     const responseBody = {
-        error: error,
-        message: error.message,
+        message: 'Ocorreu um erro interno no servidor. Tente novamente mais tarde ou contate o suporte.',
         // requestId: req.requestId,
     };
+
+    // só mostra mensagem do erro se não for erro de sintaxe ou de servidor
+    if (status < 500) {
+        responseBody.message = error.message;
+    }
 
     if ('auth' in error) {
         responseBody.auth = error.auth;
     }
-
-    // temporário, verificar pra depois colocar logs estruturados
-    console.log(error);
 
     res.status(status).json(responseBody);
 }

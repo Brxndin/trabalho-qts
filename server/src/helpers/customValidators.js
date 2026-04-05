fa9e77665f0870b8cd623055c59ce4ddb073cee7
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-04-05 19:37:56 -0300

    Novos helpers para facilitar o desenvolvimento

--- server/src/helpers/customValidators.js
+++ server/src/helpers/customValidators.js
@@ -13,3 +13,29 @@ export function filtraDadosPermitidos(data, dadosPermitidos) {
 
     return dadosFiltrados;
 }
+
+export function multiConcat(separador, ...strings) {
+    let stringsFiltradas = [];
+
+    for (const string of strings) {
+        if (string !== null && string !== undefined && string !== '') {
+            stringsFiltradas.push(string);
+        }
+    }
+
+    return stringsFiltradas.join(separador);
+}
+
+export function isEmptyObject(object) {
+    return object && Object.keys(object).length === 0;
+}
+
+export function isEmail() {
+    // verificar se vale a pena usar regex pra validar ou usar biblioteca
+    // acho que não faz sentido baixar biblioteca só pra isso
+}
+
+export function isCPFValido() {
+    // verificar pra usar uma função padrão
+    // deve existir biblioteca, mas baixar dependência só pra isso não faz muito sentido
+}

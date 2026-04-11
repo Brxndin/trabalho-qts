0858ef5152bdac46d1391e76b4620ac26cfbbd66
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-04-11 19:42:47 -0300

    Melhoria no formulário de Consultas

--- app/src/pages/ConsultaForm.jsx
+++ app/src/pages/ConsultaForm.jsx
@@ -17,17 +17,18 @@ export default function ConsultaForm() {
       <FormLayout
         title={'Cadastro de Consultas'}
         componentsAndNames={[
-          { label: "CPF do Médico", name: "cpfMedico", type: "text", placeholder: "Informe o CPF do Médico", required: true },
-          { label: "Nome do Médico", name: "nomeMedico", type: "text", placeholder: "Nome do Médico", disabled: true },
-          { label: "CRM do Médico", name: "crm", type: "text", placeholder: "CRM do Médico", disabled: true },
-          { label: "CPF do Paciente", name: "cpfPaciente", type: "text", placeholder: "Informe o CPF do Paciente", required: true },
-          { label: "Nome do Paciente", name: "nomePaciente", type: "text", placeholder: "Nome do Paciente", disabled: true },
-          { label: "Peso", name: "peso", type: "text", required: true },
-          { label: "Temperatura", name: "temperatura", type: "text", required: true },
+          { label: "Código", name: "codigo", type: "text", placeholder: "Código da consulta", disabled: true },
+          { label: "CPF do Médico", name: "medicoCPF", type: "text", placeholder: "Informe o CPF do médico", required: true },
+          { label: "Nome do Médico", name: "medicoNome", type: "text", placeholder: "Nome do médico", disabled: true },
+          { label: "CRM do Médico", name: "crm", type: "text", placeholder: "CRM do médico", disabled: true },
+          { label: "CPF do Paciente", name: "pacienteCPF", type: "text", placeholder: "Informe o CPF do paciente", required: true },
+          { label: "Nome do Paciente", name: "pacienteNome", type: "text", placeholder: "Nome do paciente", disabled: true },
+          { label: "Peso", name: "peso", placeholder: "Informe o peso do paciente", type: "text", required: true },
+          { label: "Temperatura", name: "temperatura", placeholder: "Informe a temperatura do paciente", type: "text", required: true },
           { label: "Data e Hora de Atendimento", name: "dataHoraAtendimento", type: "datetime-local", value: dayjs().format('YYYY-MM-DD HH:mm:ss'), disabled: true },
-          { label: "Descrição dos Sintomas", name: "descricaoSintomas", type: "textarea", required: true },
-          { label: "Diagnóstico e Tratamento Sugerido", name: "diagnosticoETratamentoSugerido", type: "textarea", required: true },
-          { label: "Status do Pagamento", name: "statusPagamento", type: "text", required: true },
+          { label: "Descrição dos Sintomas", name: "descricaoSintomas", placeholder: "Descreva os sintomas do paciente", type: "textarea", required: true },
+          { label: "Diagnóstico e Tratamento Sugerido", name: "diagnosticoETratamentoSugerido", placeholder: "Descreva o diagnóstico e o tratamento sugerido para o paciente", type: "textarea", required: true },
+          { label: "Status do Pagamento", name: "statusPagamento", placeholder: "Informe o status do pagamento", type: "text", required: true },
           // verificar dados restantes
         ]}
         submitButtonText={id ? 'Atualizar' : 'Cadastrar'}

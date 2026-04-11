9189b85ae96c77a63241f5f8acdb721b4d2743d7
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-04-10 23:57:00 -0300

    Nível de acesso para a Home

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../auth/AuthContext";
+
 export default function Home() {
+  const navigate = useNavigate();
+  const { authState } = useAuth();
+
+  useEffect(() => {
+    if (!authState?.auth) {
+      navigate("/forbidden");
+    }
+  }, [authState, navigate]);
+
   return (
     <div>
       <h2>Bem-vindo à Clínica de Cardiologia!</h2>

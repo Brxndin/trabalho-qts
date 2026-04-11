8933dc822916e8c93528c70d23675510b31d097a
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-04-11 17:47:39 -0300

    Ajustes e melhorias no formulário de consultas

--- app/src/pages/ConsultaForm.jsx
+++ app/src/pages/ConsultaForm.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import FormLayout from "../components/FormLayout";
 import { useAuth } from "../auth/AuthContext";
+import dayjs from "dayjs";
 
 export default function ConsultaForm() {
   const { id } = useParams();
@@ -10,8 +11,6 @@ export default function ConsultaForm() {
   // aqui terá umas funcionalidades especiais
   // dados do médico já deverão vir carregados (é possível verificando quem está logado)
   // ao preencher cpf do paciente e tirar o foco do campo, deverá buscar os dados dele para preenchimento ou avisar que ele não existe
-  // Esses campos de preenchimento automático devem estar bloqueados para edição, desenvolver isso no form padrão
-  // data deverá ser a atual por padrão
 
   return (
     <div>
@@ -19,11 +18,16 @@ export default function ConsultaForm() {
         title={'Cadastro de Consultas'}
         componentsAndNames={[
           { label: "CPF do Médico", name: "cpfMedico", type: "text", placeholder: "Informe o CPF do Médico", required: true },
-          { label: "Nome do Médico", name: "nomeMedico", type: "text", placeholder: "Nome do Médico", required: true },
-          { label: "CRM do Médico", name: "crm", type: "text", placeholder: "CRM do Médico", required: true },
+          { label: "Nome do Médico", name: "nomeMedico", type: "text", placeholder: "Nome do Médico", disabled: true },
+          { label: "CRM do Médico", name: "crm", type: "text", placeholder: "CRM do Médico", disabled: true },
           { label: "CPF do Paciente", name: "cpfPaciente", type: "text", placeholder: "Informe o CPF do Paciente", required: true },
-          { label: "Nome do Paciente", name: "nomePaciente", type: "text", placeholder: "Nome do Paciente", required: true },
-          { label: "Data e Hora de Atendimento", name: "data_hora_atendimento", type: "date", required: true },
+          { label: "Nome do Paciente", name: "nomePaciente", type: "text", placeholder: "Nome do Paciente", disabled: true },
+          { label: "Peso", name: "peso", type: "text", required: true },
+          { label: "Temperatura", name: "temperatura", type: "text", required: true },
+          { label: "Data e Hora de Atendimento", name: "dataHoraAtendimento", type: "datetime-local", value: dayjs().format('YYYY-MM-DD HH:mm:ss'), disabled: true },
+          { label: "Descrição dos Sintomas", name: "descricaoSintomas", type: "textarea", required: true },
+          { label: "Diagnóstico e Tratamento Sugerido", name: "diagnosticoETratamentoSugerido", type: "textarea", required: true },
+          { label: "Status do Pagamento", name: "statusPagamento", type: "text", required: true },
           // verificar dados restantes
         ]}
         submitButtonText={id ? 'Atualizar' : 'Cadastrar'}
@@ -32,9 +36,9 @@ export default function ConsultaForm() {
         linkGetData={'/consultas'}
         linkUpdate={'/consultas'}
         authPermission={true}
-        // aqui define que somente funcionários podem cadastrar e editar
-        createPermission={authState?.user?.role.includes(4)}
-        updatePermission={authState?.user?.role.includes(4)}
+        // aqui define que somente médicos podem cadastrar e editar
+        createPermission={authState?.user?.role.includes(2)}
+        updatePermission={authState?.user?.role.includes(2)}
       />
     </div>
   );

51598425b737061cb57979607ff7cda4ddc34599
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-04-12 01:25:31 -0300

    Formatação

--- app/src/components/FormLayout.jsx
+++ app/src/components/FormLayout.jsx
@@ -118,9 +118,7 @@ export default function FormLayout({
           return (
             <div>
               <label>{value.label}</label>
-              {["text", "email", "password", "datetime-local", "date"].includes(
-                value.type,
-              ) && (
+              {["text", "email", "password", "datetime-local", "date"].includes(value.type) && (
                 <input
                   type={value.type}
                   name={value.name}

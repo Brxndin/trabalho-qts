8fc0e31a11fc05205e73b05c593c08dc1dac4cc4
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-04-12 00:17:19 -0300

    Formatação

--- app/src/components/AxiosInterceptor.jsx
+++ app/src/components/AxiosInterceptor.jsx
@@ -1,16 +1,16 @@
-import { useNavigate } from 'react-router-dom';
-import { useLayoutEffect } from 'react';
-import { setupInterceptors } from '../config/api';
+import { useNavigate } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { setupInterceptors } from "../config/api";
 
 // feito nesse formato para o interceptor se comportar como componente
 const AxiosInterceptor = ({ children }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useLayoutEffect(() => {
-        setupInterceptors(navigate);
-    }, [navigate]);
+  useLayoutEffect(() => {
+    setupInterceptors(navigate);
+  }, [navigate]);
 
-    return children;
+  return children;
 };
 
 export default AxiosInterceptor;

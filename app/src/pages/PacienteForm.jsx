d3d36d85e52860b6dece60b65eb2b4d353b32c02
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2026-05-19 22:11:32 -0300

    Remoção de campos que o paciente não tem

--- app/src/pages/PacienteForm.jsx
+++ app/src/pages/PacienteForm.jsx
@@ -14,12 +14,7 @@ export default function PacienteForm() {
         componentsAndNames={[
           { label: "Nome", name: "nome", type: "text", placeholder: "Informe o nome", required: true },
           { label: "CPF", name: "cpf", type: "text", placeholder: "Informe o CPF", required: true },
-          { label: "Ocupação", name: "funcao", type: "text", placeholder: "Informe a ocupação", required: true },
           { label: "Telefone", name: "telefone", type: "text", placeholder: "Informe o telefone" },
-          { label: "Endereço", name: "endereco", type: "text", placeholder: "Rua ou avenida", required: true },
-          { label: "Número", name: "numero", type: "text", placeholder: "Número", required: true },
-          { label: "Bairro", name: "bairro", type: "text", placeholder: "Bairro", required: true },
-          { label: "CEP", name: "cep", type: "text", placeholder: "CEP", required: true },
           { label: "Data de Nascimento", name: "dataNascimento", type: "date", required: true },
           { label: "E-mail", name: "email", type: "email", placeholder: "Informe o e-mail", required: true },
         ]}
